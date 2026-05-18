0627bcb716a5faa3f4bca55f1663a162ef286b02
suckerson <shson1849@gmail.com>
2026-05-18 14:01:34 +0900

    first commit

---
+++ src/main/java/son/suck/music9/Music9Application.java
@@ -0,0 +1,13 @@
+package son.suck.music9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Music9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Music9Application.class, args);
+	}
+
+}
---
+++ src/test/java/son/suck/music9/Music9ApplicationTests.java
@@ -0,0 +1,13 @@
+package son.suck.music9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Music9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
