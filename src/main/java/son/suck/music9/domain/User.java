f59aa621677c803d2e5aebe64a5b00994778db4c
suckerson <shson1849@gmail.com>
2026-05-18 16:25:31 +0900

    user table

---
+++ src/main/java/son/suck/music9/domain/User.java
@@ -0,0 +1,42 @@
+package son.suck.music9.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+//@AllArgsConstructor
+@NoArgsConstructor
+//@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.createdDate = LocalDateTime.now();
+    }
+
+}
